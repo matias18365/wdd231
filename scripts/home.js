8fffb5bd38892552fe1f403233fc562271ce4f69
gut23014@byui.edu <matias1805ga@gmail.com>
2025-01-13 21:09:35 -0300

    THIS ARE ALL THE CHANGES

---
+++ scripts/home.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector('#currentyear');
+    const currentYear = new Date();
+    yearSpan.innerText = currentYear.getFullYear();
+
+    const lastModifiedSpan = document.querySelector('#lastModified');
+    lastModifiedSpan.innerText = document.lastModified;
+});
+
+//This is for the menu button.
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
