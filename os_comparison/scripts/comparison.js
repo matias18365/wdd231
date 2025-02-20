7501255df1634254a0ea526be26142d6929caca0
gut23014@byui.edu <matias1805ga@gmail.com>
2025-02-19 23:36:17 -0400

    I am working on the js file to display the content of the operating system of the json file

--- os_comparison/scripts/comparison.js
+++ os_comparison/scripts/comparison.js
@@ -15,4 +15,56 @@ document.addEventListener("DOMContentLoaded", function() {
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const osList = document.getElementById("os-list");
+    const osUrl = 'https://matias18365.github.io/wdd231/os_comparison/data/comparison.js'; // URL to JSON
+
+    // Function to fetch member data
+    async function fetchOs() {
+        try {
+            const response = await fetch(osUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            displayOs(data.os);
+        } catch (error) {
+            console.error('Error fetching os data:', error);
+        }
+    }
+    fetchOs();
+
+    // Function to display os's on the page
+    function displayOs(os) {
+
+        console.log(os)
+        os.forEach(osy => {
+            const osCard = document.createElement('div');
+            osCard.className = 'os-card';
+            osCard.innerHTML = `
+                <img src="${osy.image}" alt="${osy.name} Logo" />
+                <h3>${osy.name}</h3>
+                <p><a href="${osy.website}" target="_blank">Visit Website</a></p>
+                <p>${osy.description}</p>
+            `;
+            osList.appendChild(osCard);
+        });
+    }
 });
